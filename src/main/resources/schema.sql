d95353c92c9aba84fd1e574a81d11a8b48d52ad8
mromanak <mroma102@gmail.com>
2023-02-09 21:50:35 -0700

    Added Title Update 4 skills and armor pieces

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -24,7 +24,7 @@ CREATE CACHED TABLE "PUBLIC"."ARMOR_PIECE"(
     "SET_BONUS_ID" VARCHAR
 );      
 ALTER TABLE "PUBLIC"."ARMOR_PIECE" ADD CONSTRAINT "PUBLIC"."CONSTRAINT_4" PRIMARY KEY("ID");   
--- 1195 +/- SELECT COUNT(*) FROM PUBLIC.ARMOR_PIECE;           
+-- 1205 +/- SELECT COUNT(*) FROM PUBLIC.ARMOR_PIECE;           
 INSERT INTO "PUBLIC"."ARMOR_PIECE" VALUES
 ('chainmail-head-lr', 0, 4, 0, 0, 0, 0, 0, 0, 0, 'Chainmail Headgear', 1, 'Chainmail', '0', 1, 0, NULL),
 ('chainmail-body-lr', 1, 4, 0, 0, 0, 0, 0, 0, 0, 'Chainmail Vest', 1, 'Chainmail', '0', 1, 0, NULL),
@@ -1251,7 +1251,17 @@ INSERT INTO "PUBLIC"."ARMOR_PIECE" VALUES
 ('chaotic-gore-magala-body-x', 1, 118, -4, -4, 2, 1, 0, 1, 0, 'Chaotic Mail/Nephilim Mail', 10, 'Chaotic Gore Magala', '2', -2, 3, NULL),
 ('chaotic-gore-magala-arms-x', 2, 118, -4, -4, 2, 0, 0, 1, 0, 'Chaotic Vambraces/Nephilim Vambraces', 10, 'Chaotic Gore Magala', '2', -2, 3, NULL),
 ('chaotic-gore-magala-waist-x', 3, 118, -4, -4, 2, 2, 0, 1, 0, 'Chaotic Faulds/Nephilim Tassets', 10, 'Chaotic Gore Magala', '2', -2, 3, NULL),
-('chaotic-gore-magala-legs-x', 4, 118, -4, -4, 2, 0, 0, 1, 0, 'Chaotic Greaves / Nephilim Greaves', 10, 'Chaotic Gore Magala', '2', -2, 3, NULL);               
+('chaotic-gore-magala-legs-x', 4, 118, -4, -4, 2, 0, 0, 1, 0, 'Chaotic Greaves / Nephilim Greaves', 10, 'Chaotic Gore Magala', '2', -2, 3, NULL),
+('velkhana-head-x', 0, 123, -1, -3, 4, 1, 0, 1, 0, 'Rimeguard Helm', 10, 'Velkhana', '2', -1, 2, NULL),
+('velkhana-body-x', 1, 123, -1, -3, 4, 1, 1, 0, 1, 'Rimeguard Mail', 10, 'Velkhana', '2', -1, 2, NULL),
+('velkhana-arms-x', 2, 123, -1, -3, 4, 1, 1, 1, 0, 'Rimeguard Gloves', 10, 'Velkhana', '2', -1, 2, NULL),
+('velkhana-waist-x', 3, 123, -1, -3, 4, 0, 1, 2, 0, 'Rimeguard Coil', 10, 'Velkhana', '2', -1, 2, NULL),
+('velkhana-legs-x', 4, 123, -1, -3, 4, 1, 0, 0, 1, 'Rimeguard Greaves', 10, 'Velkhana', '2', -1, 2, NULL),
+('risen-valstrax-head-x', 0, 121, 3, -2, -2, 0, 1, 0, 1, 'Valstrax Helm - Epoch', 10, 'Risen Valstrax', '2', -2, -2, NULL),
+('risen-valstrax-body-x', 1, 121, 3, -2, -2, 0, 0, 0, 2, 'Valstrax Mail - Epoch', 10, 'Risen Valstrax', '2', -2, -2, NULL),
+('risen-valstrax-arms-x', 2, 121, 3, -2, -2, 2, 0, 0, 1, 'Valstrax Braces - Epoch', 10, 'Risen Valstrax', '2', -2, -2, NULL),
+('risen-valstrax-waist-x', 3, 121, 3, -2, -2, 2, 0, 0, 1, 'Valstrax Coil - Epoch', 10, 'Risen Valstrax', '2', -2, -2, NULL),
+('risen-valstrax-legs-x', 4, 121, 3, -2, -2, 0, 0, 1, 1, 'Valstrax Greaves - Epoch', 10, 'Risen Valstrax', '2', -2, -2, NULL);               
 CREATE CACHED TABLE "PUBLIC"."ARMOR_PIECE_SKILL"(
     "SKILL_LEVEL" INTEGER NOT NULL CHECK (("SKILL_LEVEL" <= 7)
     AND ("SKILL_LEVEL" >= 1)),
@@ -1259,7 +1269,7 @@ CREATE CACHED TABLE "PUBLIC"."ARMOR_PIECE_SKILL"(
     "SKILL_ID" VARCHAR NOT NULL
 );      
 ALTER TABLE "PUBLIC"."ARMOR_PIECE_SKILL" ADD CONSTRAINT "PUBLIC"."CONSTRAINT_F" PRIMARY KEY("ARMOR_PIECE_ID", "SKILL_ID");     
--- 2450 +/- SELECT COUNT(*) FROM PUBLIC.ARMOR_PIECE_SKILL;     
+-- 2477 +/- SELECT COUNT(*) FROM PUBLIC.ARMOR_PIECE_SKILL;     
 INSERT INTO "PUBLIC"."ARMOR_PIECE_SKILL" VALUES
 (1, 'chainmail-head-lr', 'botanist'),
 (1, 'chainmail-head-lr', 'defense-boost'),
@@ -3735,7 +3745,34 @@ INSERT INTO "PUBLIC"."ARMOR_PIECE_SKILL" VALUES
 (2, 'chaotic-gore-magala-waist-x', 'resentment'),
 (1, 'chaotic-gore-magala-legs-x', 'berserk'),
 (1, 'chaotic-gore-magala-legs-x', 'bloodlust'),
-(3, 'chaotic-gore-magala-legs-x', 'coalescence');             
+(3, 'chaotic-gore-magala-legs-x', 'coalescence'),
+(1, 'velkhana-head-x', 'protective-polish'),
+(3, 'velkhana-head-x', 'ammo-up'),
+(3, 'velkhana-head-x', 'grinder-s'),
+(1, 'velkhana-body-x', 'frostcraft'),
+(3, 'velkhana-body-x', 'quick-sheath'),
+(1, 'velkhana-arms-x', 'frostcraft'),
+(3, 'velkhana-arms-x', 'critical-draw'),
+(1, 'velkhana-waist-x', 'frostcraft'),
+(3, 'velkhana-waist-x', 'focus'),
+(1, 'velkhana-legs-x', 'element-exploit'),
+(3, 'velkhana-legs-x', 'critical-element'),
+(5, 'velkhana-legs-x', 'ice-attack'),
+(1, 'risen-valstrax-head-x', 'attack-boost'),
+(1, 'risen-valstrax-head-x', 'dragon-conversion'),
+(2, 'risen-valstrax-head-x', 'latent-power'),
+(1, 'risen-valstrax-body-x', 'attack-boost'),
+(1, 'risen-valstrax-body-x', 'dragon-conversion'),
+(3, 'risen-valstrax-body-x', 'dragon-resistance'),
+(1, 'risen-valstrax-arms-x', 'attack-boost'),
+(2, 'risen-valstrax-arms-x', 'critical-boost'),
+(2, 'risen-valstrax-arms-x', 'latent-power'),
+(1, 'risen-valstrax-waist-x', 'attack-boost'),
+(1, 'risen-valstrax-waist-x', 'dragon-conversion'),
+(3, 'risen-valstrax-waist-x', 'stamina-surge'),
+(1, 'risen-valstrax-legs-x', 'critical-boost'),
+(2, 'risen-valstrax-legs-x', 'attack-boost'),
+(3, 'risen-valstrax-legs-x', 'stun-resistance');    
 CREATE CACHED TABLE "PUBLIC"."JEWEL"(
     "ID" VARCHAR NOT NULL,
     "JEWEL_LEVEL" INT DEFAULT 1 NOT NULL CHECK (("JEWEL_LEVEL" >= 1)
@@ -3784,7 +3821,7 @@ CREATE CACHED TABLE "PUBLIC"."SKILL"(
     "NAME" VARCHAR NOT NULL
 );          
 ALTER TABLE "PUBLIC"."SKILL" ADD CONSTRAINT "PUBLIC"."CONSTRAINT_4B" PRIMARY KEY("ID");        
--- 139 +/- SELECT COUNT(*) FROM PUBLIC.SKILL;  
+-- 141 +/- SELECT COUNT(*) FROM PUBLIC.SKILL;  
 INSERT INTO "PUBLIC"."SKILL" VALUES
 ('attack-boost', 'Increases attack power.', 7, 7, 'Attack Boost'),
 ('agitator', 'Increases attack power and affinity when large monsters become enraged.', 5, 5, 'Agitator'),
@@ -3927,14 +3964,16 @@ INSERT INTO "PUBLIC"."SKILL" VALUES
 ('wind-mantle', 'Using Silkbind attacks boosts Wirebug Gauge recovery. Landing continuous attacks increases the effect. Wears off when you sheathe your weapon.', 1, 1, 'Wind Mantle'),
 ('powder-mantle', 'Continuously attacking a large monster will cause powder to form around you. The color and effects of the powder will change over time.', 1, 1, 'Powder Mantle'),
 ('strife', 'Increases elemental attack and affinity in proportion to the length of your red health gauge.', 1, 1, 'Strife'),
-('berserk', 'Switch to the Blue Swap Scroll and all vitality will change to red gauge and start decreasing. As long as you have red, you cannot faint.', 1, 1, 'Berserk');       
+('berserk', 'Switch to the Blue Swap Scroll and all vitality will change to red gauge and start decreasing. As long as you have red, you cannot faint.', 1, 1, 'Berserk'),
+('frostcraft', 'An aura of frost builds when your weapon is sheathed, raising attack. The effect decreases with each melee hit or shot/arrow fired.', 1, 1, 'Frostcraft'),
+('dragon-conversion', 'When using the Red Swap Scroll, all elemental resistance is set to 0 and converted into elemental attack. The Blue Swap Scroll raises elemental resistance.', 1, 1, 'Dragon Conversion');           
 CREATE CACHED TABLE "PUBLIC"."SKILL_EFFECT"(
     "SKILL_ID" VARCHAR NOT NULL,
     "EFFECT" VARCHAR NOT NULL,
     "SKILL_LEVEL" INTEGER NOT NULL
 );             
 ALTER TABLE "PUBLIC"."SKILL_EFFECT" ADD CONSTRAINT "PUBLIC"."CONSTRAINT_B" PRIMARY KEY("SKILL_ID", "SKILL_LEVEL");             
--- 434 +/- SELECT COUNT(*) FROM PUBLIC.SKILL_EFFECT;           
+-- 436 +/- SELECT COUNT(*) FROM PUBLIC.SKILL_EFFECT;           
 INSERT INTO "PUBLIC"."SKILL_EFFECT" VALUES
 ('attack-boost', 'Attack +3', 1),
 ('attack-boost', 'Attack +6', 2),
@@ -4376,7 +4415,9 @@ INSERT INTO "PUBLIC"."SKILL_EFFECT" VALUES
 ('wind-mantle', 'Activate effect.', 1),
 ('powder-mantle', 'Get sent flying when wreathed in red, and you''ll receive explosive damage. Attack when in blue for an explosive finish.', 1),
 ('strife', 'Increases attack power in accordance to gauge length.', 1),
-('berserk', 'Activate effect.', 1);      
+('berserk', 'Activate effect.', 1),
+('frostcraft', 'Activate effect.', 1),
+('dragon-conversion', 'Activate effect. All elemental resistances +3.', 1);   
 CREATE CACHED TABLE "PUBLIC"."UNCAPPING_SKILLS"(
     "UNCAPPING_SKILL_ID" VARCHAR,
     "ID" VARCHAR NOT NULL
